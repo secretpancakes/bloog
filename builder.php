7be102ae4432a5a3c26788a79ade46f2b81aa04e
Duane Jeffers <duane@duanejeffers.com>
2014-08-22 04:16:14 -0500

    Stub for builder Builder is the tool used to prepare a release for flattening.

---
+++ builder.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+	 * Bloog v0.2 (Namespace Rewrite)
+	 * This is a rewrite of the original bloog at http://github.com/duanejeffers/bloog
+	 *
+	 * (c) Copyright 2014 Duane Jeffers / Secret Pancakes (http://github.secretpancak.es/bloog/)
+	 *				 2013 Duane Jeffers (duane@duanejeffers.com)
+	 *
+	 * See LICENSE File for License information.
+	 */
+
+	// This file parses the application and flattens it.
